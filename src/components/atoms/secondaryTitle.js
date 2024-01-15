33495b71c694a341ec8fcfd1fa43e144c2f73926
Iskren Minkov <iskratayt1231@gmail.com>
2024-01-15 21:05:19 +0200

    Minor Change

--- src/components/atoms/secondaryTitle.js
+++ src/components/atoms/secondaryTitle.js
@@ -7,6 +7,30 @@ const SecondaryTitle = styled.h2`
   font-weight: 800;
   margin: ${(props) => props.margin || '0'};
   padding: ${(props) => props.padding || '0'};
+
+  @media (max-width: 1536px) {
+    font-size: 38px;
+  }
+
+  @media (max-width: 1280px) {
+    font-size: 36px;
+  }
+
+  @media (max-width: 1024px) {
+    font-size: 36px;
+  }
+
+  @media (max-width: 768px) {
+    font-size: 34px;
+  }
+
+  @media (max-width: 640px) {
+    font-size: 32px;
+  }
+
+  @media (max-width: 475px) {
+    font-size: 30px;
+  }
 `
 
 export default SecondaryTitle
